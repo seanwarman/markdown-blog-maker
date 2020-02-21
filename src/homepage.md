e0ee1eaf9e493d7b889371f2fdcb747d93b971fa
Sean <cd3k@pop-os.localdomain>
2020-02-21 22:22:27 +0000

    wrote some content for the homepage and restyled everything

---
+++ src/homepage.md
@@ -0,0 +1,20 @@
+# Stuff That's Tough
+
+My name's Sean Warman and I've been a developer since 2018. 
+Here's some tips and tricks I've learned mainly so I
+don't forget them myself.
+
+This project is a kind of learning process as I'm
+getting to know Webpack and AWS better.
+
+Each post is kept as a markdown file in a folder on my computer
+and then built using webpack and deployed automatically as I
+save any file in the project.
+
+I like the idea of keeping a database of sorts that exists as
+a folder of files on an s3 bucket rather than as obfuscated 
+binary code on a server.
+
+I've made a controller that will read the folder, list all the
+items in it and render them in the **Posts** section on the 
+right, it also makes a uri for each one.
--- src/index.js
+++ src/index.js
@@ -1,52 +1,67 @@
 import './style.css';
 import showdown from 'showdown';
+import homepage from './homepage.md'
 
 const importAll = (r) => r.keys().map(r)
-const vimFiles = importAll(require.context('./vim', false, /\.md$/))
+const posts = importAll(require.context('./posts', false, /\.md$/))
   .sort().reverse()
 
-async function component(md) {
+async function renderPost(md, paths) {
   const res = await fetch(md)
   const text = await res.text()
   const converter = new showdown.Converter();
 
   const div = document.createElement('div');
-  div.innerHTML = converter.makeHtml(text);
+
+  div.innerHTML = `
+
+    ${
+      window.location.pathname != '/' ?
+        `<a 
+        class="back"
+        href="${window.location.origin}"
+      >back</a>`
+        :
+      ''
+    }
+
+    <div class="sider">
+
+      <ul>
+        <h3>Posts</h3>
+        ${paths.map(path => `
+          <li>
+            <a href="${window.location.origin + '/' + path}">
+              ${path}
+            </a>
+          </li>
+        `).join('')}
+      </ul>
+
+    </div>
+
+    ${converter.makeHtml(text)}
+
+  `
+
   document.body.appendChild(div);
-}
-
-function routes(paths) {
-  const ul = document.createElement('ol');
-  ul.innerHTML = `${paths.map(path => `
-
-  <h1>Contents</h1>
-    <li>
-      <a href="${window.location.href + path}">
-        ${path}
-      </a>
-    </li>`
-
-  ).join('')}`
-
-  document.body.appendChild(ul);
-
 }
 
 let index = 0
 let paths = []
 let fileNames = []
 
-for (let file of vimFiles) {
+for (let file of posts) {
   paths.push(file.default.slice(0, file.default.indexOf('.md')))
 }
 
 if(window.location.pathname === '/') {
   // A hardcoded homepage
-  routes(paths)
+  renderPost(homepage, paths)
 } else {
   for(let path of paths) {
     if(window.location.pathname === '/' + path) {
-      component(path+'.md')
+      renderPost(path+'.md', paths)
       break
     }
   }
