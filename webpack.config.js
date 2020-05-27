5ba6250ac488acdb5d7ea9721349c8c2303a3cb0
Sean <seanwarman@protonmail.com>
2020-05-27 05:26:16 +0100

    added favicon

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,8 @@ module.exports = {
   },
   plugins: [ 
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
     }),
     new GoogleFontsPlugin({fonts: [
       { family: "Nunito" },
