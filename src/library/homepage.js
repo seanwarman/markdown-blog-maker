1849105b0f774597a8b4509e7998615f12ebac01
Sean <seanwarman@protonmail.com>
2021-02-20 18:54:43 +0000

    updated homepage copy

--- src/library/homepage.js
+++ src/library/homepage.js
@@ -1,18 +1,6 @@
 export default `
 
-My name's Sean Warman and I've been a developer since 2018. 
-Here's some tips and tricks I've learned, partly so I
-don't forget them myself.
+A blog for development tips and tricks mainly around frontend Javascript. There's also lots of Vim stuff here (as my obsession gets more and more out of hand).
 
-Each post is kept as a markdown file in a folder on my computer
-and then built using webpack and deployed automatically as I
-save any file in the project.
-
-I like the idea of keeping a database of sorts that exists as
-a folder of files on an s3 bucket rather than as obfuscated 
-binary code on a server.
-
-I've made a controller that will read the folder, list all the
-items in it and render them in the section on the 
-right, it also makes a uri for each one.
+Sean Warman
 `
